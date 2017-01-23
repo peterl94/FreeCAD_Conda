0cb1429ede5da3af9421a5e7a3c3c685b6509af7
looooo <sppedflyer@gmail.com>
2017-01-23 05:04:00 +0100

    faster builds

--- FreeCAD/build.sh
+++ FreeCAD/build.sh
@@ -1,58 +1,61 @@
 FREECAD_SOURCE=~/projects/FreeCAD # this is the path to the FreeCAD source (modyfy to your needs)
-mkdir $PREFIX/lib/freecad -p
+BUILD_FULL=false                  # run cmake .. if true, if not use the --dirty flag of conda (maybe this can be automated)
 
 
-VERBOSE=1 cmake -DCMAKE_BUILD_TYPE=Debug \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DNETGENDATA=$PREFIX/include/netgen/libsrc \
-      -DNETGEN_INCLUDEDIR=$PREFIX/include/netgen/nglib \
-      -DNGLIB_INCLUDE_DIR=$PREFIX/include/netgen/nglib \
-      -DOCC_INCLUDE_DIR=$PREFIX/include/opencascade \
-      -DOCC_LIBRARY_DIR=$PREFIX/lib \
-      -DOCC_LIBRARIES=$PREFIX/lib CACHE PATH \
-      -DFREECAD_USE_OCC_VARIANT="Official Version" \
-      -DOCC_OCAF_LIBRARIES=$PREFIX/lib CACHE PATH \
-      -DSWIG_DIR=$PREFIX/share/swig/3.0.8 \
-      -DSWIG_EXECUTABLE=$PREFIX/bin/swig \
-      -DPYTHON_EXECUTABLE=$PYTHON \
-      -DBUILD_FEM_NETGEN=YES \
-      -DBUILD_ARCH=YES \
-      -DBUILD_ASSEMBLY=NO \
-      -DBUILD_COMPLETE=YES \
-      -DBUILD_DRAFT=YES \
-      -DBUILD_DRAWING=YES \
-      -DBUILD_FEM=YES \
-      -DBUILD_GUI=YES \
-      -DBUILD_IDF=YES \
-      -DBUILD_IMAGE=YES \
-      -DBUILD_IMPORT=YES \
-      -DBUILD_INSPECTION=YES \
-      -DBUILD_JTREADER=NO \
-      -DBUILD_MATERIAL=YES \
-      -DBUILD_MESH=YES \
-      -DBUILD_MESH_PART=YES \
-      -DBUILD_OPENSCAD=YES \
-      -DBUILD_PART=YES \
-      -DBUILD_PART_DESIGN=YES \
-      -DBUILD_PATH=YES \
-      -DBUILD_POINTS=YES \
-      -DBUILD_PLOT=YES \
-      -DBUILD_RAYTRACING=YES \
-      -DBUILD_REVERSEENGINEERING=YES \
-      -DBUILD_ROBOT=YES \
-      -DBUILD_SANDBOX=YES \
-      -DBUILD_SHIP=YES \
-      -DBUILD_SKETCHER=YES \
-      -DBUILD_SPREADSHEET=YES \
-      -DBUILD_START=YES \
-      -DBUILD_TEMPLATE=YES \
-      -DBUILD_TEST=YES \
-      -DBUILD_VR=NO \
-      -DBUILD_WEB=YES ${FREECAD_SOURCE}
+if [ "$BUILD_FULL" = true ] ; then
+    VERBOSE=1 cmake -DCMAKE_BUILD_TYPE=Debug \
+          -DCMAKE_INSTALL_PREFIX=$PREFIX \
+          -DCMAKE_PREFIX_PATH=$PREFIX \
+          -DNETGENDATA=$PREFIX/include/netgen/libsrc \
+          -DNETGEN_INCLUDEDIR=$PREFIX/include/netgen/nglib \
+          -DNGLIB_INCLUDE_DIR=$PREFIX/include/netgen/nglib \
+          -DOCC_INCLUDE_DIR=$PREFIX/include/opencascade \
+          -DOCC_LIBRARY_DIR=$PREFIX/lib \
+          -DOCC_LIBRARIES=$PREFIX/lib CACHE PATH \
+          -DFREECAD_USE_OCC_VARIANT="Official Version" \
+          -DOCC_OCAF_LIBRARIES=$PREFIX/lib CACHE PATH \
+          -DSWIG_DIR=$PREFIX/share/swig/3.0.8 \
+          -DSWIG_EXECUTABLE=$PREFIX/bin/swig \
+          -DPYTHON_EXECUTABLE=$PYTHON \
+          -DBUILD_FEM_NETGEN=ON \
+          -DBUILD_ARCH=ON \
+          -DBUILD_ASSEMBLY=NO \
+          -DBUILD_COMPLETE=ON \
+          -DBUILD_DRAFT=ON \
+          -DBUILD_DRAWING=ON \
+          -DBUILD_FEM=ON \
+          -DBUILD_GUI=YES \
+          -DBUILD_IDF=ON \
+          -DBUILD_IMAGE=ON \
+          -DBUILD_IMPORT=ON \
+          -DBUILD_INSPECTION=ON \
+          -DBUILD_JTREADER=OFF \
+          -DBUILD_MATERIAL=ON \
+          -DBUILD_MESH=ON \
+          -DBUILD_MESH_PART=ON \
+          -DBUILD_OPENSCAD=ON \
+          -DBUILD_PART=YES \
+          -DBUILD_PART_DESIGN=ON \
+          -DBUILD_PATH=ON \
+          -DBUILD_POINTS=ON \
+          -DBUILD_PLOT=ON \
+          -DBUILD_RAYTRACING=ON \
+          -DBUILD_REVERSEENGINEERING=ON \
+          -DBUILD_ROBOT=ON \
+          -DBUILD_SANDBOX=ON \
+          -DBUILD_SHIP=ON \
+          -DBUILD_SKETCHER=ON \
+          -DBUILD_SPREADSHEET=ON \
+          -DBUILD_START=ON \
+          -DBUILD_TEMPLATE=ON \
+          -DBUILD_TEST=ON \
+          -DBUILD_VR=NO \
+          -DBUILD_WEB=ON \
+          -DBUILD_TECHDRAW=ON ${FREECAD_SOURCE}
+fi
 
-make -j3 2>&1 | tee output.txt
-make install
+make -j5 2>&1 | tee output.txt
+make install -j5
 # mkdir $PREFIX/bin -p
 # ln -s ../lib/freecad/bin/FreeCAD $PREFIX/bin/freecad
 # ln -s ../lib/freecad/bin/FreeCADCmd $PREFIX/bin/FreeCADCmd
